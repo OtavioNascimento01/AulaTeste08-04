4cffedce2cef02bb978033d5b3cec3094bca2585
Otávio Nascimento <otaviohenriquedonascimento@outlook.com>
2025-04-08 19:45:51 -0300

    Aula Teste 08-04

---
+++ script.js
@@ -0,0 +1,33 @@
+document.getElementById('submitButton').addEventListener('click', function() {
+    const name = document.getElementById('nameInput').value;
+    const messageElement = document.getElementById('message');
+    
+    if (name) {
+      messageElement.textContent = `Olá, ${name}! Bem-vindo ao nosso site!`;
+      messageElement.style.color = 'green';
+    } else {
+      messageElement.textContent = 'Por favor, digite seu nome!';
+      messageElement.style.color = 'red';
+    }
+  });
+
+  document.addEventListener('DOMContentLoaded',()=>{
+    fetch('data.json')
+    .then(response=>response.json())
+    .then(data=>{
+      const contentDiv = document.getElementById('content');
+      contentDiv.innerHTML = `<p>${data.message}</p>`;
+    })
+    .catch(error=>console.error('Erro ao carregar o JSON:', error))
+  })
+
+  document.addEventListener('DOMContentLoaded', ()=>{
+    fetch('http://localhost:3000/data')
+    .then(response=>response.json())
+    .then(data=>{
+      const contentDiv = document.getElementById('content');
+      contentDiv.innerHTML = data.map(item=>
+        `<p>${item.coluna}</p>`).join('');
+    })
+    .catch(error=>console.error('Erro ao carregar os dados:', error));
+  })
